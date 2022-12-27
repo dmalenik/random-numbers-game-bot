1e25cc11a5db8a36193b401a3b5af824acc546cc
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-27 11:08:39 +0100

    refactor: uncomment code and delete main.html

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const bot = new Bot(token);
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running!"));
 bot.command("greeting", (ctx) => ctx.reply("Hello, world!"));
-// bot.on("message", (ctx) => {
-//   console.log(ctx);
-//   ctx.reply("Got another message!");
-// });
+bot.on("message", (ctx) => {
+  console.log(ctx);
+  ctx.reply("Got another message!");
+});
 bot.start();
