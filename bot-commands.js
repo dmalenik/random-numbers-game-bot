f35a0ee9f4c025cbaf3b2609698aa28c1901bd4a
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-27 15:52:38 +0100

    feat(bot-commands): set bot commands

---
+++ bot-commands.js
@@ -0,0 +1,13 @@
+const {
+  getMyCommandsList,
+  deleteMyCommandsList,
+  setMyCommandsList,
+} = require("./bot-commands-manager.js");
+
+deleteMyCommandsList();
+getMyCommandsList();
+setMyCommandsList(
+  { command: "start", description: "Start the bot" },
+  { command: "greeting", description: "Say hello to the world!" },
+  { command: "info", description: "Get info about the user" }
+);
