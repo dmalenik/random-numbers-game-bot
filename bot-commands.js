34c0e57c1f96f7546ddf43cd9868ec0612be0470
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-27 16:38:20 +0100

    refactor: remove deleteMyCommandList and getMyCommandList

--- bot-commands.js
+++ bot-commands.js
@@ -4,11 +4,10 @@ const {
   setMyCommandsList,
 } = require("./bot-commands-manager.js");
 
-deleteMyCommandsList();
-getMyCommandsList();
 setMyCommandsList(
   { command: "start", description: "Start the bot" },
   { command: "greeting", description: "Say hello to the world!" },
   { command: "info", description: "Get info about the user" },
   { command: "game", description: "Start a random numbers game" }
 );
+
