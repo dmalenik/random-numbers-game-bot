cdb417d78a88970a3c4a1307c160c1ae55aafe1a
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-28 17:48:58 +0100

    feat(command handlers): create bot commands handlers object

--- botCommandsHandlers.js
+++ botCommandsHandlers.js
@@ -1,19 +1,18 @@
 module.exports.botCommandsHandlers = {
-  handleStart: async function handleStart(context) {
-    let chatId = context.chat.id;
-
-    await context.api.sendPhoto(
-      chatId,
+  handleStart: function (context, chatID) {
+    context.api.sendPhoto(
+      chatID,
       "https://tlgrm.eu/_/stickers/6a3/497/6a34971d-6648-37c2-8f2b-8940f65ba906/8.jpg"
     );
-    await context.api.sendMessage(chatId, "Hi, Dima!");
+    context.api.sendMessage(chatID, "Hi, Dima!");
   },
-  handleInfo: async function handleInfo(context) {
-    let chatId = context.chat.id;
-
-    await context.api.sendMessage(
-      chatId,
+  handleInfo: function (context, chatID) {
+    context.api.sendMessage(
+      chatID,
       `Your name is ${context.from.first_name} ${context.from.last_name}`
     );
+  },
+  handleDefault: function (context, chatID) {
+    context.api.sendMessage(chatID, "Sorry, I don't know this command");
   },
 };
