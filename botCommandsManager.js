338fe5dc2017f4e086d649e25b6a98ae5a417745
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-28 17:47:51 +0100

    refactor(commands manager): remove names from commands helper functions

--- botCommandsManager.js
+++ botCommandsManager.js
@@ -1,19 +1,19 @@
 const { bot } = require("./bot.js");
 
 module.exports.botCommandsManager = {
-  getMyCommandsList: async function getMyCommandsList() {
+  getMyCommandsList: async function () {
     let response = await bot.api.getMyCommands();
     let myCommandsList = JSON.parse(JSON.stringify(response));
 
     console.log(myCommandsList);
   },
-  deleteMyCommandsList: async function deleteMyCommandsList() {
+  deleteMyCommandsList: async function () {
     let response = await bot.api.deleteMyCommands();
     let state = JSON.parse(JSON.stringify(response));
 
     console.log(state);
   },
-  setMyCommandsList: async function setMyCommandsList(commands) {
+  setMyCommandsList: async function (commands) {
     let response = await bot.api.setMyCommands(commands);
     let myCommands = JSON.parse(JSON.stringify(response));
 
