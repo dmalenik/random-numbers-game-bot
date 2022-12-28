67457f8416191391029fed00cbe1c18bd8f65bfa
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-28 14:34:25 +0100

    refactor(botCommands): create a list of bot commands

--- bot-commands.js
+++ bot-commands.js
@@ -3,11 +3,13 @@ const {
   deleteMyCommandsList,
   setMyCommandsList,
 } = require("./bot-commands-manager.js");
-
-setMyCommandsList(
+const commands = [
   { command: "start", description: "Start the bot" },
   { command: "greeting", description: "Say hello to the world!" },
   { command: "info", description: "Get info about the user" },
-  { command: "game", description: "Start a random numbers game" }
-);
+  { command: "game", description: "Start a random numbers game" },
+];
 
+setMyCommandsList(commands);
+
+module.exports.commands = commands;
