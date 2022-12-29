a054cf526716e12b34443f9dd48a161364711110
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-29 10:38:24 +0100

    refactor(messages): add switch to different cases

--- messages.js
+++ messages.js
@@ -3,17 +3,21 @@ const { commands } = require("./data.js");
 const { botCommandsHandlers } = require("./botCommandsHandlers.js");
 
 const [start, greeting, info, game] = commands;
+const { handleStart, handleInfo, handleDefault } = botCommandsHandlers;
 
 bot.on("message", (ctx) => {
   let text = ctx.message.text;
   let chatId = ctx.chat.id;
 
-  if (text === `/${start.command}`) {
-    botCommandsHandlers.handleStart(ctx, chatId);
-  } else if (text === `/${info.command}`) {
-    botCommandsHandlers.handleInfo(ctx, chatId);
-  } else {
-    botCommandsHandlers.handleDefault(ctx, chatId);
+  switch (text) {
+    case `/${start.command}`:
+      handleStart(ctx, chatId);
+      break;
+    case `/${info.command}`:
+      handleInfo(ctx, chatId);
+      break;
+    default:
+      handleDefault(ctx, chatId);
   }
 });
 
