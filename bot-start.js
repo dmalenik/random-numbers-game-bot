ae9dcbf2f3807f01920a7c27fd60d99809a6a845
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-30 21:51:12 +0100

    feat: implement DB, gameOptions

---
+++ bot-start.js
@@ -0,0 +1,30 @@
+const { bot } = require("./bot.js");
+const { gameOptions } = require("./gameOptions.js");
+const { commands } = require("./data.js");
+const { botCommandsHandlers } = require("./botCommandsHandlers.js");
+const { DB } = require("./DB.js");
+
+const [start, info, game] = commands;
+const { handleStart, handleInfo, handleDefault, handleGame } =
+  botCommandsHandlers;
+
+bot.on("message", (ctx) => {
+  let text = ctx.message.text;
+  let chatId = ctx.chat.id;
+
+  switch (text) {
+    case `/${start.command}`:
+      handleStart(ctx, chatId);
+      break;
+    case `/${game.command}`:
+      handleGame(ctx, chatId, gameOptions, DB);
+      break;
+    case `/${info.command}`:
+      handleInfo(ctx);
+      break;
+    default:
+      handleDefault(ctx);
+  }
+});
+
+bot.start();
