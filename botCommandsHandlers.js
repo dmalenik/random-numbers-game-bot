32f5e67d20fb01f38534d0173c67d2896f9e4a48
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-30 21:52:05 +0100

    refactor(send messages): replace sendMessage to reply

--- botCommandsHandlers.js
+++ botCommandsHandlers.js
@@ -1,17 +1,34 @@
-module.exports.botCommandsHandlers = {
+const botCommandsHandlers = {
   handleStart: function (context, chatID) {
     context.api.sendPhoto(
       chatID,
       "https://tlgrm.eu/_/stickers/6a3/497/6a34971d-6648-37c2-8f2b-8940f65ba906/8.jpg"
     );
     setTimeout(() => {
-      context.api.sendMessage(chatID, "Hi, Dima!");
+      context.reply(`Hi, ${context.from.first_name}!`);
     }, 100);
   },
-  handleInfo: function (context, chatID) {
-    context.api.sendMessage(chatID, `Your name is ${context.from.first_name}`);
+  handleGame: function (context, chatID, gameOptions, DB) {
+    context.reply("Try to guess the number from 0 to 9");
+
+    let randomNumber = Math.floor(Math.random() * 10);
+
+    DB[chatID] = randomNumber;
+
+    return setTimeout(
+      () =>
+        context.reply("Try to guess", {
+          reply_markup: gameOptions,
+        }),
+      100
+    );
   },
-  handleDefault: function (context, chatID) {
-    context.api.sendMessage(chatID, "Sorry, I don't know this command");
+  handleInfo: function (context) {
+    context.reply(`Your name is ${context.from.first_name}`);
+  },
+  handleDefault: function (context) {
+    context.reply("Sorry, I don't know this command");
   },
 };
+
+module.exports.botCommandsHandlers = botCommandsHandlers;
