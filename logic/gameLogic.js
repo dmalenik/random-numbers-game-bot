625fc4e9bc303f7fdffdd864a026064bb938f012
Dmitriy Malenik <dmalenik@gmail.com>
2023-01-02 13:11:06 +0100

    refactor: rename a function, change parameters list and add a new logic

--- logic/gameLogic.js
+++ logic/gameLogic.js
@@ -1,30 +1,26 @@
 const gameLogic = {
-  handleGame: function (context, chatID, gameOptions, DB) {
-    context.reply("Try to guess the number from 0 to 9");
+  handleGame: function (ctx, chatID, DB, gameInterface) {
+    ctx.reply("Try to guess the number from 0 to 9");
 
     let randomNumber = Math.floor(Math.random() * 10);
 
     DB[chatID] = randomNumber;
 
     return setTimeout(() => {
-      context.reply("Try to guess", {
-        reply_markup: gameOptions,
+      ctx.reply("Try to guess", {
+        reply_markup: gameInterface,
       });
     }, 100);
   },
-  handleGameCallbackQuery: function (context, row, column, DB, playAgainBtn) {
-    let button =
-      context.update.callback_query.message.reply_markup.inline_keyboard[row][
-        column
-      ];
-    let text = button.text;
-    let current = parseInt(text, 10);
-    let [toCompare] = Object.values(DB);
-    if (current === toCompare) {
-      context.reply("You win!");
+  respondToCallbackQuery: function (ctx, data, randomNumber, playAgainBtn) {
+    if (data === randomNumber) {
+      ctx.reply("You win!");
     } else {
-      context.reply("You lose!");
-      context.reply("Try again", { reply_markup: playAgainBtn });
+      ctx.reply("You lose!");
+
+      return ctx.reply("Play again", {
+        reply_markup: playAgainBtn,
+      });
     }
   },
 };
