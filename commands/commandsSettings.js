8a5a7ee83a43466929671d2e21e278ef1fe6161c
Dmytro Malienik <dmalenik@gmail.com>
2024-01-09 21:42:41 +0300

    refactor(commands): remove folder

--- commands/commandsSettings.js
+++
@@ -1,4 +0,0 @@
-const { setMyCommandsList } = require('./commandsManager.js')
-const { commands } = require('./list.js')
-
-setMyCommandsList(commands)
--- commands/list.js
+++
@@ -1,7 +0,0 @@
-const commands = [
-    { command: 'start', description: 'Start the bot' },
-    { command: 'info', description: 'Get info about the user' },
-    { command: 'game', description: 'Start a random numbers game' },
-]
-
-module.exports.commands = commands
