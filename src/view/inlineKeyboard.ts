cad2d4a576d1821a7fbb54b96f37568f3a480eb4
Dmytro Malienik <dmalenik@gmail.com>
2024-01-10 14:51:00 +0300

    feat: create inline keyboard

---
+++ src/view/inlineKeyboard.ts
@@ -0,0 +1,18 @@
+import { InlineKeyboard } from 'grammy'
+
+const inlineKeyboard = new InlineKeyboard()
+    .text('1')
+    .text('2')
+    .text('3')
+    .row()
+    .text('4')
+    .text('5')
+    .text('6')
+    .row()
+    .text('7')
+    .text('8')
+    .text('9')
+    .row()
+    .text('0')
+
+export default inlineKeyboard
