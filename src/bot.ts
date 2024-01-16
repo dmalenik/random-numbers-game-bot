f820af3ffe9c693d2fde2eef0bcc5a8616d923e5
Dmytro Malienik <dmalenik@gmail.com>
2024-01-16 17:47:22 +0300

    feat(token): place sensitive data inside .env file
    - (.gitignore): place .env file in gitignore
    - (package.json): install dotenv module
    - (bot.ts): replace token variable with process.env.TOKEN env variable

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,12 @@
 import { Bot, session } from 'grammy'
-import token from './data'
 import initial from './initial'
 import start from './middleware/start'
 import play from './middleware/play'
 import onCallbackQueryData from './middleware/onCallbackQueryData'
 import type { MyContext } from './types/MyContext'
+import 'dotenv/config'
 
-const bot = new Bot<MyContext>(token)
+const bot = new Bot<MyContext>(process.env.TOKEN!)
 
 bot.use(session({ initial }))
 
