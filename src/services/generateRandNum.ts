6b301af14b6184b835465db2470d5ecc7fd4da14
Dmytro Malienik <dmalenik@gmail.com>
2024-01-17 18:07:12 +0300

    refactor(generateRandNum): move module to services

--- src/services/generateRandNum.ts
+++ src/services/generateRandNum.ts
@@ -1,3 +1,12 @@
 interface GenerateRandNumFn {
     (min: number, max: number): number
 }
+
+const generateRandNum: GenerateRandNumFn = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+export default generateRandNum
