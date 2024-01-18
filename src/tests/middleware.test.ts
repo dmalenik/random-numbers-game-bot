33223938cec40df8edf682371fe067510ac6e8a5
Dmytro Malienik <dmalenik@gmail.com>
2024-01-18 22:27:27 +0300

    refactor(middleware tests): optimize test modules
    - remove play test file
    - rename start test file on middleware
    - test start and play middlewares in one file

--- src/tests/middleware.test.ts
+++ src/tests/middleware.test.ts
@@ -1,7 +1,9 @@
+import start from '../middleware/start'
 import play from '../middleware/play'
 
-describe('checking the play middleware', () => {
+describe('checking the middleware', () => {
     test('is of type function', () => {
+        expect(typeof start).toBe('function')
         expect(typeof play).toBe('function')
     })
 })
--- src/tests/start.test.ts
+++
@@ -1,7 +0,0 @@
-import start from '../middleware/start'
-
-describe('checking the start middleware', () => {
-    test('is of type function', () => {
-        expect(typeof start).toBe('function')
-    })
-})
