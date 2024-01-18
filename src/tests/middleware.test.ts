04aec9505a8959af6722a8aaedf3e380543d582e
Dmytro Malienik <dmalenik@gmail.com>
2024-01-18 23:14:12 +0300

    refactor(middleware tests): optimize testing for onCallbackQueryData

--- src/tests/middleware.test.ts
+++ src/tests/middleware.test.ts
@@ -1,9 +1,11 @@
 import start from '../middleware/start'
 import play from '../middleware/play'
+import onCallbackQueryData from '../middleware/onCallbackQueryData'
 
 describe('checking the middleware', () => {
     test('is of type function', () => {
         expect(typeof start).toBe('function')
         expect(typeof play).toBe('function')
+        expect(typeof onCallbackQueryData).toBe('function')
     })
 })
--- src/tests/onCallbackQueryData.test.ts
+++
@@ -1,7 +0,0 @@
-import onCallbackQueryData from '../middleware/onCallbackQueryData'
-
-describe('checking onCallbackQueryData', () => {
-    test('is of type function', () => {
-        expect(typeof onCallbackQueryData).toBe('function')
-    })
-})
