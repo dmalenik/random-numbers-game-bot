d47f1885977397f6bb1a97b2ec1c577e75b7b256
Dmytro Malienik <dmalenik@gmail.com>
2024-01-18 23:14:48 +0300

    refactor(generate random numbers function tests): optimize test cases

--- src/tests/generateRandNum.test.ts
+++ src/tests/generateRandNum.test.ts
@@ -1,11 +1,10 @@
 import generateRandNum from '../services/generateRandNum'
 
 describe('checking random numbers generator function', () => {
-    const rand = generateRandNum(0, 9)
-    test('if rand is greater then or equal to 0', () => {
+    test('if rand is in a range between 0 and 9', () => {
+        const rand = generateRandNum(0, 9)
+
         expect(rand).toBeGreaterThanOrEqual(0)
-    })
-    test('if rand is less then or equal to 9', () => {
         expect(rand).toBeLessThanOrEqual(9)
     })
 })
