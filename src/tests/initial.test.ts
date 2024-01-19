e80283d51ef83ee697c754b439ccb389dacab132
Dmytro Malienik <dmalenik@gmail.com>
2024-01-19 11:53:32 +0300

    refactor(initial tests): check if the function return the appropriate value

--- src/tests/initial.test.ts
+++ src/tests/initial.test.ts
@@ -4,4 +4,13 @@ describe('checking initial function', () => {
     test('is of type function', () => {
         expect(typeof initial).toBe('function')
     })
+
+    test('return value is appropriate', () => {
+        const mockFn = jest.fn(initial)
+
+        expect(mockFn()).toEqual({
+            rand: 0,
+            tries: 1,
+        })
+    })
 })
