855fecf6eb22daafa40bb37c53a8b052c595568d
Dmytro Malienik <dmalenik@gmail.com>
2024-01-19 14:37:20 +0300

    feat(on callback query data): implement a choice feature
    - a gamer can choose whether to play a new game or finish the current one

--- src/middleware/onCallbackQueryData.ts
+++ src/middleware/onCallbackQueryData.ts
@@ -1,15 +1,17 @@
 import type { Context, SessionFlavor } from 'grammy'
 import bot from '../bot'
 import gameKeyboard from '../view/gameKeyboard'
-import inlineKeyboard from '../view/inlineKeyboard'
+import choiceKeyboard from '../view/choiceKeyboard'
 
 const data = {
     info: 'You win! Number of tries:',
     memeURL: 'https://media1.tenor.com/m/mvegoURYtSUAAAAC/jonah-hill-omg.gif',
-    action: 'Try again',
+    tryAgain: 'Try again',
+    playAgain: 'Press /play command again to start a new game',
+    choice: ['Yes!', 'No?'],
 }
 
-const { info, memeURL, action } = data
+const { info, memeURL, tryAgain, playAgain, choice } = data
 
 interface SessionData {
     rand: number
@@ -20,15 +22,33 @@ type MyContext = Context & SessionFlavor<SessionData>
 
 const onCallbackQueryData = (ctx: MyContext) => {
     const data = ctx.update.callback_query?.data ?? ''
-    const pressed = Number.parseInt(data)
+    const state = choice.includes(data) ? 'choice' : 'game'
+    let pressed = null
 
-    if (pressed === ctx.session.rand) {
-        ctx.reply(`${info} ${ctx.session.tries}`)
-        ctx.replyWithAnimation(memeURL)
-        bot.stop()
-    } else {
-        ctx.session.tries++
-        ctx.reply(action, { reply_markup: inlineKeyboard })
+    switch (state) {
+        case 'game':
+            pressed = Number.parseInt(data)
+
+            if (pressed === ctx.session.rand) {
+                ctx.reply(`${info} ${ctx.session.tries}`)
+                ctx.replyWithAnimation(memeURL)
+                ctx.reply('Would you like to make another try?', {
+                    reply_markup: choiceKeyboard,
+                })
+            } else {
+                ctx.session.tries++
+                ctx.reply(tryAgain, { reply_markup: gameKeyboard })
+            }
+            break
+        case 'choice':
+            pressed = data
+
+            if (pressed === 'Yes!') {
+                ctx.reply(playAgain)
+            } else {
+                bot.stop()
+            }
+            break
     }
 }
 
