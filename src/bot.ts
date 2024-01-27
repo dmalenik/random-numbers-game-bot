8844ae113a30962e484e5cf4d4c94c5fce1e21f6
Dmytro Malienik <dmalenik@gmail.com>
2024-01-27 17:47:12 +0300

    feat(pg adapter): implement pgsql adapter

--- src/bot.ts
+++ src/bot.ts
@@ -4,25 +4,49 @@ import initial from './helpers/initial'
 import start from './middleware/start'
 import play from './middleware/play'
 import onCallbackQueryData from './middleware/onCallbackQueryData'
+import { PsqlAdapter } from '@grammyjs/storage-psql'
+import { Client } from 'pg'
 import 'dotenv/config'
 
 interface SessionData {
     rand: number
     tries: number
 }
-
 type MyContext = Context & SessionFlavor<SessionData>
 
 const { token } = process.env
 const bot = new Bot<MyContext>(token!)
 
-bot.use(session({ initial }))
+const client = new Client({
+    user: 'postgres',
+    host: '127.0.0.1',
+    database: 'sessions',
+    password: '123456',
+    port: 5432,
+})
 
-bot.command('start', start)
-bot.command('play', play)
+const init = async () => {
+    await client.connect()
 
-bot.on('callback_query:data', onCallbackQueryData)
+    bot.use(
+        session({
+            initial,
+            storage: await PsqlAdapter.create({
+                tableName: 'session',
+                client,
+            }),
+        })
+    )
 
-bot.start()
+    bot.command('start', start)
+    bot.command('play', play)
+
+    bot.on('callback_query:data', onCallbackQueryData)
+
+    bot.start()
+}
+
+init()
 
 export default bot
+export { client }
