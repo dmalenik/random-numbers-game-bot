bcb9590dcccacb6fcb176d1d7ccc2a268d525929
Dmytro Malienik <dmalenik@gmail.com>
2024-01-27 17:48:21 +0300

    refactor(on callback query): rename keys

--- src/middleware/onCallbackQueryData.ts
+++ src/middleware/onCallbackQueryData.ts
@@ -1,10 +1,10 @@
 import type { Context, SessionFlavor } from 'grammy'
-import bot from '../bot'
+import bot, { client } from '../bot'
 import gameKeyboard from '../view/gameKeyboard'
 import choiceKeyboard from '../view/choiceKeyboard'
 
 const data = {
-    info: 'You win! Number of tries:',
+    youWin: 'You win! Number of tries:',
     memeURL: 'https://media1.tenor.com/m/mvegoURYtSUAAAAC/jonah-hill-omg.gif',
     tryAgain: 'Try again',
     choiceQuestion: 'Would you like to make another try?',
@@ -13,14 +13,13 @@ const data = {
     goodbye: 'Ok! Bye then',
 }
 
-const { info, memeURL, tryAgain, choiceQuestion, choices, newGame, goodbye } =
+const { youWin, memeURL, tryAgain, choiceQuestion, choices, newGame, goodbye } =
     data
 
 interface SessionData {
     rand: number
     tries: number
 }
-
 type MyContext = Context & SessionFlavor<SessionData>
 
 const onCallbackQueryData = async (ctx: MyContext) => {
@@ -34,7 +33,7 @@ const onCallbackQueryData = async (ctx: MyContext) => {
                 pressed = Number.parseInt(data)
 
                 if (pressed === ctx.session.rand) {
-                    await ctx.reply(`${info} ${ctx.session.tries}`)
+                    await ctx.reply(`${youWin} ${ctx.session.tries}`)
                     await ctx.replyWithAnimation(memeURL)
                     await ctx.reply(choiceQuestion, {
                         reply_markup: choiceKeyboard,
@@ -51,11 +50,14 @@ const onCallbackQueryData = async (ctx: MyContext) => {
         case 'choice':
             try {
                 pressed = data
+                // make a copy of current stats
+                // place them in stats DB
 
                 if (pressed === 'Yes!') {
                     await ctx.reply(newGame)
                 } else {
                     await ctx.reply(goodbye)
+                    await client.end()
                     await bot.stop()
                 }
             } catch (error) {
